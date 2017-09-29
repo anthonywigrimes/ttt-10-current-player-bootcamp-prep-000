b0640684719921f9997780d0f8f1b28cf0fd3933
Anthony Grimes <agrimes@charter.net>
2017-09-29 18:43:17 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,10 @@
 def turn(board)
     counter = 0
-    board.each
+    board.each do |current|
+        if current == "X" || current == "0"
+            counter += 1
+        end
+    end
 end
 
 def current_player(board)
